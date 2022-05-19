d3242a2b8c4e719d1839a5623ea585d2539256f4
Triyono <yono.tri@gmail.com>
2022-05-19 09:27:15 +0700

    fungsi

---
+++ fungsi.py
@@ -0,0 +1,15 @@
+# print("hello")
+# jumlah_karakter = len("hello world")
+# print(jumlah_karakter)
+
+def fungsi_saya():
+    print('selamat pagi')
+    print('nama saya triyono')
+    print('hari ini belajar python')
+
+
+print('ini bagian 4')
+
+
+fungsi_saya()
+fungsi_saya()
